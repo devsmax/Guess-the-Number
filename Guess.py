f74d3b7cf649ee21fc27790fa9542dd5f073ae43
Max <devsmax@outlook.com>
2015-10-29 12:49:34 +0100

    NumCheck
    Checks if number is in range

--- Guess.py
+++ Guess.py
@@ -2,3 +2,6 @@ from random import randint
 
 number = randint(0,100)
 guess = int(raw_input("Guess the Number between 0 and 100:"))
+
+if number > 100 or number < 0:
+    print "That number is out of range!"
