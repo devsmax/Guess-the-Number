13060d5531b51b67d5d1d83d1f7f18d4cee13959
Max <devsmax@outlook.com>
2015-10-29 14:08:05 +0100

    WORK
    I made it work and i tested it

--- Guess.py
+++ Guess.py
@@ -1,20 +1,22 @@
 from random import randint
 
 number = randint(0,100)
+#print (number)
+for turn in range(50):
 
-for turn in range():
-
-    guess = int(raw_input("Guess the Number between 0 and 100:"))
+    guess = int(input("Guess the Number between 0 and 100:"))
 
     if number > 100 or number < 0:
-        print "That number is out of range!"
+        print ("That number is out of range!")
     else:
         if number == guess:
-            print "Congratulations, you have guessed The Number"
-            print "The number was" number
+            print ("Congratulations, you have guessed The Number")
+            print ("The number was " + str(number))
+            input("Press Enter to exit...")
+            exit()
         elif number <= guess:
-            print "Higher"
+            print ("Lower")
         elif number >= guess:
-            print "Lower"
+            print ("Higher")
         else:
-            print "You done goofed"
+            print ("You done goofed")
