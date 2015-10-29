53634c198c1313ddec5953c7de45e1a4a825445e
Max <devsmax@outlook.com>
2015-10-29 12:57:55 +0100

    Turn system
    Makes it so that players can guess more then one time

--- Guess.py
+++ Guess.py
@@ -1,17 +1,20 @@
 from random import randint
 
 number = randint(0,100)
-guess = int(raw_input("Guess the Number between 0 and 100:"))
 
-if number > 100 or number < 0:
-    print "That number is out of range!"
+for turn in range():
 
-if number == guess:
-    print "Congratulations, you have guessed The Number"
-    print "The number was" number
-elif number <= guess:
-    print "Higher"
-elif number >= guess:
-    print "Lower"
-else:
-    print "You done goofed"
+    guess = int(raw_input("Guess the Number between 0 and 100:"))
+
+    if number > 100 or number < 0:
+        print "That number is out of range!"
+    else:
+        if number == guess:
+            print "Congratulations, you have guessed The Number"
+            print "The number was" number
+        elif number <= guess:
+            print "Higher"
+        elif number >= guess:
+            print "Lower"
+        else:
+            print "You done goofed"
