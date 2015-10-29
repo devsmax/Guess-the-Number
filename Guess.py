592d0b97843559616210f0479b1808c006873466
Max <devsmax@outlook.com>
2015-10-29 12:50:08 +0100

    Game
    This checks if the number is correct

--- Guess.py
+++ Guess.py
@@ -5,3 +5,13 @@ guess = int(raw_input("Guess the Number between 0 and 100:"))
 
 if number > 100 or number < 0:
     print "That number is out of range!"
+
+if number == guess:
+    print "Congratulations, you have guessed The Number"
+    print "The number was" number
+elif number <= guess:
+    print "Higher"
+elif number >= guess:
+    print "Lower"
+else:
+    print "You done goofed"
