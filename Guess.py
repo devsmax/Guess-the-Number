80c2262cc2ce7338becf322d24b787ae98ba629e
Max <devsmax@outlook.com>
2015-10-30 15:33:49 +0100

    Text output
    Fixed the spacing between messages and added some spaces at the input
    lines.

--- Guess.py
+++ Guess.py
@@ -2,20 +2,23 @@
 
 number = randint(0,100)
 #print (number)
-name = input("What is your name?")
+name = input("What is your name? ")
 print ("Okay, " + str(name) + ", let's do this!")
 
 for turn in range(50):
 
-    guess = int(input("Guess the Number between 0 and 100:"))
+    guess = int(input("Guess the Number between 0 and 100: "))
 
     if number > 100 or number < 0:
         print ("That number is out of range!")
     else:
         if number == guess:
+            print ("")
             print ("Congratulations," + str(name) + ", you have guessed the number")
+            print ("")
             print ("It took you " + str(turn + 1) + " turns to guess it.")
-            print ("The number was " + str(number))
+            print ("The number was " + str(number) + ".")
+            print ("")
             input("Press Enter to exit...")
             exit()
         elif number <= guess:
