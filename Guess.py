7e530ede5cf8d66661b4b311c3961b77d8a04ff6
Max <devsmax@outlook.com>
2016-01-08 14:40:43 +0100

    Loop
    Replaced the turn statement with a loop and fixed  the turn counting
    system.

--- Guess.py
+++ Guess.py
@@ -4,23 +4,23 @@ number = randint(0,100)
 #print (number)
 name = input("What is your name? ")
 print ("Okay, " + str(name) + ", let's do this!")
-
-for turn in range(50):
+turn = 0
+while True:
 
     guess = int(input("Guess the Number between 0 and 100: "))
-
+    turn += 1
     if number > 100 or number < 0:
         print ("That number is out of range!")
     else:
         if number == guess:
             print ("")
-            print ("Congratulations, " + str(name) + " , you have guessed the number")
+            print ("Congratulations, " + str(name) + ", you have guessed the number")
             print ("")
-            print ("It took you " + str(turn + 1) + " turns to guess it.")
+            print ("It took you " + str(turn) + " turns to guess it.")
             print ("The number was " + str(number) + ".")
             print ("")
             input("Press Enter to exit...")
-            exit()
+            break
         elif number <= guess:
             print ("Lower")
         elif number >= guess:
